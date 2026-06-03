9fd703cea3b66600233ee2094fa7259407b4594b
Adithyainfo <sreedurgavenkatadithya@gmail.com>
2026-06-03 19:06:42 +0530

    Add GSAP animations for header, paragraph, and button

---
+++ script.js
@@ -0,0 +1,17 @@
+gsap.from("h1",{
+  y:-100,
+  opacity:0,
+  duration:1.5
+});
+
+gsap.from("p",{
+  opacity:0,
+  duration:2,
+  delay:1
+});
+
+gsap.from("button",{
+  scale:0,
+  duration:1,
+  delay:1.5
+});
